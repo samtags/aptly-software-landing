9c425f24db4135009d43dd2bb722acbd404d9e82
samtags <sam.tags@protonmail.ch>
2026-03-02 19:30:59 +0800

    feat: carousel tablet and desktop views

--- src/app/page.js
+++ src/app/page.js
@@ -10,52 +10,64 @@ export default function Home() {
         <Navbar />
       </div>
       <Hero />
-      <div className="pt-18 pb-8 overflow-x-hidden">
-        <FramerDiv
-          className="flex gap-4"
-          animate={{ x: ["0%", "-183.5%"] }}
-          transition={{
-            repeat: Infinity,
-            repeatType: "loop",
-            duration: 15,
-            ease: "linear",
-          }}
-        >
-          <Carousel
-            title="Vertax Labs (1)"
-            description="Web design • development"
-            rate="120%"
-            rateDescription="Boost in leads"
-            imageUrl="/png/YRBacVkY1hNjQzvJVDb24A3JOM.png"
-          />
-          <Carousel
-            title="Vertax Labs (2)"
-            description="Web design • development"
-            rate="120%"
-            rateDescription="Boost in leads"
-            imageUrl="/png/LWaKedMy6DtEAbrBCG2JzD13Tc.png"
-          />
-          <Carousel
-            title="Vertax Labs (3)"
-            description="Web design • development"
-            rate="120%"
-            rateDescription="Boost in leads"
-            imageUrl="/png/GSJQypQMYu1yzL9SeVvdATXD6tY.png"
-          />
-          <Carousel
-            title="Vertax Labs (1)"
-            description="Web design • development"
-            rate="120%"
-            rateDescription="Boost in leads"
-          />
-          <Carousel
-            title="Vertax Labs (2)"
-            description="Web design • development"
-            rate="120%"
-            rateDescription="Boost in leads"
-          />
-        </FramerDiv>
-      </div>
+      <section id="projects" className="pt-18 pb-8 overflow-x-hidden lg:mt-1">
+        <div className="projects-fade">
+          <FramerDiv
+            matchMedia="(max-width: 810px)"
+            className="flex gap-4"
+            animate={{ x: ["0%", "-183.5%"] }}
+            transition={{
+              repeat: Infinity,
+              repeatType: "loop",
+              duration: 15,
+              ease: "linear",
+            }}
+          >
+            <Carousel
+              title="Vertax Labs (1)"
+              description="Web design • development"
+              rate="120%"
+              rateDescription="Boost in leads"
+              imageUrl="/png/YRBacVkY1hNjQzvJVDb24A3JOM.png"
+            />
+            <Carousel
+              title="Vertax Labs (2)"
+              description="Web design • development"
+              rate="120%"
+              rateDescription="Boost in leads"
+              imageUrl="/png/LWaKedMy6DtEAbrBCG2JzD13Tc.png"
+            />
+            <Carousel
+              title="Vertax Labs (3)"
+              description="Web design • development"
+              rate="120%"
+              rateDescription="Boost in leads"
+              imageUrl="/png/GSJQypQMYu1yzL9SeVvdATXD6tY.png"
+            />
+            <Carousel
+              title="Vertax Labs (1)"
+              description="Web design • development"
+              rate="120%"
+              rateDescription="Boost in leads"
+              imageUrl="/png/YRBacVkY1hNjQzvJVDb24A3JOM.png"
+            />
+            <Carousel
+              title="Vertax Labs (2)"
+              description="Web design • development"
+              rate="120%"
+              rateDescription="Boost in leads"
+              imageUrl="/png/LWaKedMy6DtEAbrBCG2JzD13Tc.png"
+            />
+            <Carousel
+              title="Vertax Labs (3)"
+              description="Web design • development"
+              rate="120%"
+              rateDescription="Boost in leads"
+              imageUrl="/png/GSJQypQMYu1yzL9SeVvdATXD6tY.png"
+            />
+          </FramerDiv>
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -6,8 +6,8 @@ export default function Carousel({
   rateDescription,
 }) {
   return (
-    <div className="bg-white rounded-[40px] border border-[#e2e7ee] p-[10px] max-w-[300px]">
-      <div className="bg-gray-100 min-w-[200px] w-full min-h-[187px] aspect-video aspect-video rounded rounded-[32px] overflow-hidden">
+    <div className="bg-white rounded-[40px] border border-[#e2e7ee] p-[10px] max-w-[300px] md:max-w-none md:min-w-[450px] lg:min-w-[550px]">
+      <div className="bg-gray-100 min-w-[200px] md:min-w-[430px] lg:min-w-[530px] w-full min-h-[187px] md:min-h-[287px] lg:min-h-[353px] aspect-video aspect-video rounded rounded-[32px] overflow-hidden">
         {imageUrl && (
           <img
             src={imageUrl}
@@ -17,15 +17,15 @@ export default function Carousel({
         )}
       </div>
       <div className="p-6">
-        <header className="text-[#0f1115] text-[18px] font-bold">
+        <header className="text-[#0f1115] text-[18px] lg:text-[20px] font-bold">
           {title}
         </header>
-        <div className="text-[#697289] text-[14px] mt-1 mb-4">
-          {description}
-        </div>
-        <div className="font-medium text-[14px]">
-          <strong>{rate}</strong>{" "}
-          <span className="text-[#697289]">{rateDescription}</span>
+        <div className="gap-4 flex flex-col md:flex-row justify-between mt-1 font-medium text-[14px]">
+          <div className="text-[#697289]">{description}</div>
+          <div>
+            <strong>{rate}</strong>{" "}
+            <span className="text-[#697289]">{rateDescription}</span>
+          </div>
         </div>
       </div>
     </div>
