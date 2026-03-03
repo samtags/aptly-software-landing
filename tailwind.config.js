fd97d48b4724ab0ab09f539d74e25f86c11d8357
samtags <sam.tags@protonmail.ch>
2026-03-04 00:16:27 +0800

    style: process section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   theme: {
+    extend: {
+      fontFamily: {
+        inter: [
+          "Inter",
+          "system-ui",
+          "-apple-system",
+          "Segoe UI",
+          "sans-serif",
+        ],
+      },
+    },
     screens: {
       ...defaultTheme.screens,
       md: "810px",
