b1db0214314927565513eb01dd55d20ac94eec33
samtags <sam.tags@protonmail.ch>
2026-03-03 19:35:49 +0800

    styles(services): web view

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -2,7 +2,10 @@ import SectionHeader from "./sectionHeader";
 import SectionTitle from "./sectionTitle";
 import { FaRibbon } from "react-icons/fa6";
 import { HiArrowUpRight } from "react-icons/hi2";
-import { RiCheckFill } from "react-icons/ri";
+import {
+  RiCheckboxBlankCircleLine,
+  RiCheckboxCircleFill,
+} from "react-icons/ri";
 
 export default function Services() {
   return (
@@ -11,23 +14,29 @@ export default function Services() {
       <SectionHeader
         main="From ideas into high-impact solutions"
         sub="That inspire and convert"
-        className="mt-4 md:max-w-[600px]"
+        className="mt-4 md:max-w-[600px] lg:max-w-[700px]"
       />
-      <div className="bg-[#f9fafb] rounded-[48px] p-5 mt-12 flex flex-col gap-5 md:grid md:grid-cols-2">
-        <div className="bg-white rounded-[32px] p-5 service-card-shadow md:order-[2] flex flex-col">
-          <div className="font-bold text-[20px]">Web & mobile apps</div>
-          <p className="mt-2 text-sm font-medium text-[#50576b]">
-            We design and develop high-performing web and mobile applications
-            that are intuitive, scalable, and visually compelling.
-          </p>
+      <div className="bg-[#f9fafb] rounded-[48px] p-5 mt-12 flex flex-col gap-5 md:grid md:grid-cols-2 lg:grid-cols-3">
+        <div className="bg-white rounded-[32px] p-7 pb-0 service-card-shadow flex flex-col justify-between md:order-[2] lg:order-[1] lg:row-span-2">
+          <div>
+            <div className="font-bold text-[20px] lg:text-[26px]">
+              Web & mobile apps
+            </div>
+            <p className="mt-2 text-sm font-medium text-[#50576b]">
+              We design and develop high-performing web and mobile applications
+              that are intuitive, scalable, and visually compelling.
+            </p>
+          </div>
           <img
             src="/png/VVMO6zTJk0afAQzH1AM75oByNQ.png"
             alt="Web & mobile apps"
             className="object-contain mt-8 bottom-fade h-[250px] md:h-[290px] lg:h-[400px]"
           />
         </div>
-        <div className="bg-white rounded-[32px] p-5 service-card-shadow md:order-[1] md:col-span-full">
-          <div className="font-bold text-[20px]">No-code websites</div>
+        <div className="bg-white rounded-[32px] p-7 service-card-shadow md:order-[1] lg:order-[2] md:col-span-full lg:col-span-2 lg:row-span-1">
+          <div className="font-bold text-[20px] lg:text-[26px]">
+            No-code websites
+          </div>
           <p className="mt-2 text-sm font-medium text-[#50576b] md:max-w-[500px]">
             Launch faster with powerful, fully customizable websites built with
             Webflow and Framer that are easy to manage and optimized for
@@ -46,51 +55,57 @@ export default function Services() {
             <Badge label="SEO Optimization" icon={<FaRibbon fontSize={14} />} />
           </div>
         </div>
-        <div className="bg-white rounded-[32px] p-5 service-card-shadow md:order-[3] flex flex-col gap-12">
-          <ul className="border border-[#e5eaf0] rounded-[18px] services-bottom-fade p-[6px] font-medium text-sm md:order-2">
+        <div className="bg-white rounded-[32px] p-7 service-card-shadow flex flex-col lg:flex-row gap-12 md:order-[3] lg:col-span-2 lg:row-span-1">
+          <ul className="border border-[#e5eaf0] flex flex-col gap-1 rounded-[18px] services-bottom-fade p-[6px] font-medium text-sm lg:text-xs md:order-2 lg:w-[260px]">
             <li className="flex justify-between items-center px-4 py-3 bg-[#f9fafb] rounded-xl">
               <span>Storyboarding</span>
-              <RiCheckFill fontSize={20} color="#ff5147" />
+              <RiCheckboxCircleFill fontSize={12} color="#ff5147" />
             </li>
             <li className="flex justify-between items-center p-4 py-1">
               <span>Style Frames & Design</span>
-              <RiCheckFill fontSize={20} color="#ff5147" />
+              <RiCheckboxCircleFill fontSize={12} color="#ff5147" />
             </li>
             <li className="flex justify-between items-center p-4 py-1">
               <span>Animation & Motion</span>
-              <RiCheckFill fontSize={20} color="#ff5147" />
+              <RiCheckboxCircleFill fontSize={12} color="#ff5147" />
             </li>
             <li className="flex justify-between items-center p-4 py-1">
               <span>Sound Design & Polish</span>
-              <RiCheckFill fontSize={20} color="#ff5147" />
+              <RiCheckboxBlankCircleLine fontSize={12} />
             </li>
             <li className="flex justify-between items-center p-4 py-1">
               <span>Review & Delivery</span>
-              <RiCheckFill fontSize={20} color="#ff5147" />
+              <RiCheckboxBlankCircleLine fontSize={12} />
             </li>
             <li className="flex justify-between items-center p-4 py-1">
               <span>Final Audit</span>
-              <RiCheckFill fontSize={20} color="#ff5147" />
+              <RiCheckboxBlankCircleLine fontSize={12} />
             </li>
           </ul>
-          <div className="md:order-1">
-            <div className="font-bold text-[20px]">
-              Motion design & animation
-            </div>
-            <p className="mt-2 text-sm font-medium text-[#50576b]">
-              Bring your brand to life with smooth, engaging motion design that
-              boost engagement.
-            </p>
-            <button className="flex justify-center items-center gap-3 bg-white text-sm font-medium pl-4 py-1 pr-1 rounded-full border border-[#d2dae4] text-[#323745] mt-5">
-              Book a call
-              <div className="bg-[#f3f5f6] p-2 rounded-full">
-                <HiArrowUpRight strokeWidth={1} fontSize={12} />
+          <div className="md:order-1 lg:flex flex-col justify-between">
+            <div>
+              <div className="font-bold text-[20px] lg:text-[26px]">
+                Motion design & animation
               </div>
-            </button>
+              <p className="mt-2 text-sm font-medium text-[#50576b]">
+                Bring your brand to life with smooth, engaging motion design
+                that boost engagement.
+              </p>
+            </div>
+            <div className="flex">
+              <button className="flex justify-center items-center gap-3 bg-white text-sm font-medium pl-4 py-1 pr-1 rounded-full border border-[#d2dae4] text-[#323745] mt-5">
+                Book a call
+                <div className="bg-[#f3f5f6] p-2 rounded-full">
+                  <HiArrowUpRight strokeWidth={1} fontSize={12} />
+                </div>
+              </button>
+            </div>
           </div>
         </div>
-        <div className="bg-white rounded-[32px] p-5 pb-0 service-card-shadow md:order-[4] md:col-span-full">
-          <div className="font-bold text-[20px]">MVP design & development</div>
+        <div className="bg-white rounded-[32px] p-7 pb-0 service-card-shadow md:order-[4] md:col-span-full lg:col-span-2">
+          <div className="font-bold text-[20px] lg:text-[26px]">
+            MVP design & development
+          </div>
           <p className="mt-2 text-sm font-medium text-[#50576b] md:max-w-[500px]">
             Validate your idea with a fast with functional, and beautifully
             designed MVP. We specialize in rapid prototyping, lean development,
@@ -104,17 +119,11 @@ export default function Services() {
             />
           </div>
         </div>
-        <div className="p-6 bg-[#151619] rounded-[32px] h-[230px] md:h-auto flex flex-col justify-end service-accent relative overflow-hidden md:order-[5] md:col-span-full">
-          <div>
-            <div className="text-white font-bold text-xl">
-              World-class Design,
-            </div>
-            <div className="text-white font-bold text-xl">
-              Data-Driver Marketing,
-            </div>
-            <div className="text-[#fff9] font-bold text-xl">
-              Unstoppable Growth
-            </div>
+        <div className="p-6 bg-[#151619] rounded-[32px] h-[230px] md:h-auto flex flex-col justify-end service-accent relative overflow-hidden md:order-[5] md:col-span-full lg:col-span-1">
+          <div className="text-xl lg:text-2xl">
+            <div className="text-white font-bold">World-class Design,</div>
+            <div className="text-white font-bold">Data-Driver Marketing,</div>
+            <div className="text-[#fff9] font-bold">Unstoppable Growth</div>
             <button className="flex justify-center items-center gap-3 bg-white text-sm font-medium pl-4 py-1 pr-1 rounded-full border border-[#d2dae4] text-[#323745] mt-5">
               Book a call
               <div className="bg-[#f3f5f6] p-2 rounded-full">
